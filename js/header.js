d32cd14101c4fbcc01ee3c978bc1af0789bf99d3
Iskander <iskander.aydynov@mail.ru>
2020-06-06 03:14:47 +0300

    Fix header and index-slider

--- js/header.js
+++ js/header.js
@@ -2,7 +2,8 @@ let listBtn = document.querySelector(".header__menu__link__list").children[0],
     list = document.querySelector("#list"),
     menu = document.querySelector(".header__menu"),
     logo = document.querySelector(".header__logo"),
-    arrow = document.querySelector(".header__menu__link__list").children[1];
+    arrow = document.querySelector(".header__menu__link__list").children[1],
+    header = document.querySelector(".header");
 
 listBtn.addEventListener("click", function (event) {
     event.preventDefault();
@@ -21,13 +22,19 @@ listBtn.addEventListener("click", function (event) {
 });
 
 window.addEventListener("scroll", function() {
-    if (window.scrollY > 110) {
+    if (window.scrollY > 100) {
         logo.classList.remove("animate__animated", "animate__fadeInDown");
         logo.classList.add("animate__animated", "animate__fadeOutUp");
         menu.style.marginTop = "-145px";
-    } else if (window.scrollY <= 110 && logo.classList.contains("animate__animated")) {
-        logo.classList.remove("animate__animated", "animate__fadeOutUp");
-        logo.classList.add("animate__animated", "animate__fadeInDown");
-        menu.style.marginTop = "";
+        if (window.scrollY > 200) {
+            header.style.height = "80px";
+        }
+    } else if (window.scrollY <= 200 && logo.classList.contains("animate__animated")) {
+        header.style.height = "225px";
+        if (window.scrollY <= 100) {
+            logo.classList.remove("animate__animated", "animate__fadeOutUp");
+            logo.classList.add("animate__animated", "animate__fadeInDown");
+            menu.style.marginTop = "";
+        }
     }
 });
