536de8a79cb9c9b0b322a40c3eb7c4966dd482e6
Iskander Aydynov <iskander.aydynov@mail.ru>
2020-12-21 15:35:13 +0300

    Перенос с одного пк на другой

--- assets/simple_mail.php
+++ assets/simple_mail.php
@@ -12,15 +12,15 @@
         //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+        $mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'iskander.aydynov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-        $mail->Password = 'Dervin1998'; // Ваш пароль от почты с которой будут отправляться письма
+        $mail->Username = 'admin@shikwood.ru'; // Ваш логин от почты с которой будут отправляться письма
+        $mail->Password = 'February2020@'; // Ваш пароль от почты с которой будут отправляться письма
         $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-        $mail->setFrom('iskander.aydynov@mail.ru'); // от кого будет уходить письмо?
-        $mail->addAddress('iaydynov1998@gmail.com');     // Кому будет уходить письмо 
+        $mail->setFrom('admin@shikwood.ru'); // от кого будет уходить письмо?
+        $mail->addAddress('admin@shikwood.ru');     // Кому будет уходить письмо 
         //$mail->addAddress('ellen@example.com');               // Name is optional
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
